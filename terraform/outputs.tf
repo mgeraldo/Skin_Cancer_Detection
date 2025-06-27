99fce4a7d00395b4a8c2619e0663ed26ac9bf874
Perry Gabriel <pgabriel@berkeley.edu>
2025-06-26 23:44:26 -0400

    Add resource_group_name output to Terraform outputs

--- terraform/outputs.tf
+++ terraform/outputs.tf
@@ -9,3 +9,7 @@ output "isic_blob_container_url" {
 output "ham_blob_container_url" {
   value = "https://${azurerm_storage_account.storage.name}.blob.core.windows.net/${var.container_names[1]}"
 }
+
+output "resource_group_name" {
+  value = azurerm_resource_group.rg.name
+}
