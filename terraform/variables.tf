3bee981b567972fc7b5a63fbe2538286a996acdd
Perry Gabriel <pgabriel@berkeley.edu>
2025-06-26 20:31:00 -0400

    Update .gitignore to include Terraform plan file and remove unused storage account name variable

--- terraform/variables.tf
+++ terraform/variables.tf
@@ -10,11 +10,6 @@ variable "resource_group_name" {
   default     = "isic2019-resource-group"
 }
 
-variable "storage_account_name" {
-  description = "The unique name of the storage account for ISIC 2019 dataset"
-  type        = string
-}
-
 variable "container_name" {
   description = "The name of the blob container for ISIC 2019 dataset"
   type        = string
