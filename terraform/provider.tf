f9776969217b457ba39d59bc80448c25c6296513
Perry Gabriel <pgabriel@berkeley.edu>
2025-06-26 20:17:19 -0400

    Add Terraform provider configuration and .gitignore for local state files

--- terraform/provider.tf
+++ terraform/provider.tf
@@ -1,10 +1,17 @@
+terraform {
+  required_providers {
+    azurerm = {
+      source  = "hashicorp/azurerm"
+      version = "~> 4.34"
+    }
+    
+    random = {
+      source  = "hashicorp/random"
+      version = "~> 3.0"
+    }
+  }
+}
+
 provider "azurerm" {
   features {}
-
-  # The following block is optional and can be used for authentication
-  # Uncomment and fill in the details if needed
-  # client_id       = var.client_id
-  # client_secret   = var.client_secret
-  # tenant_id       = var.tenant_id
-  # subscription_id = var.subscription_id
 }
